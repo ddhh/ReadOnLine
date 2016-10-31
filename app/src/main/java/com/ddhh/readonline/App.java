de0d2544ea4e0de237295cba7a3064dd7f216672
ddhh <976087885@qq.com>
2016-10-31 23:37:10 +0800

    加入第三方分享功能（未完善）

--- app/src/main/java/com/ddhh/readonline/App.java
+++ app/src/main/java/com/ddhh/readonline/App.java
@@ -2,13 +2,14 @@ package com.ddhh.readonline;
 
 import android.app.Application;
 
+import cn.sharesdk.framework.ShareSDK;
+
 public class App extends Application{
-	
-	
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		ShareSDK.initSDK(this,"188761fd38930");
 	}
 
 }
--- app/src/main/java/com/ddhh/readonline/activity/MainActivity.java
+++ app/src/main/java/com/ddhh/readonline/activity/MainActivity.java
@@ -23,8 +23,11 @@ import com.ddhh.readonline.R;
 import com.ddhh.readonline.presenter.MainPresenter;
 import com.ddhh.readonline.view.MainView;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 public class MainActivity extends AppCompatActivity
-        implements MainView, View.OnClickListener,NavigationView.OnNavigationItemSelectedListener {
+        implements MainView, View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
     EditText queryEdit;
     ImageButton searchBtn;
@@ -78,7 +81,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
-
+            showShare();
         } else if (id == R.id.nav_send) {
 
         }
@@ -88,12 +91,40 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
+        oks.setTitle("标题");
+        // titleUrl是标题的网络链接，QQ和QQ空间等使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+        // 启动分享GUI
+        oks.show(this);
+    }
+
     @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.search_btn)
+        if (v.getId() == R.id.search_btn)
             presenter.startActivity();
-        else{
-            Intent intent = new Intent(this,TVListActivity.class);
+        else {
+            Intent intent = new Intent(this, TVListActivity.class);
             startActivity(intent);
         }
     }
@@ -153,9 +184,10 @@ public class MainActivity extends AppCompatActivity
 
         }
     };
+
     @Override
     public String getQueryString() {
-        if(queryEdit!=null){
+        if (queryEdit != null) {
             return queryEdit.getText().toString();
         }
 
@@ -164,7 +196,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void startActivity(String query) {
-        Intent intent = new Intent(this,ResultListActivity.class);
+        Intent intent = new Intent(this, ResultListActivity.class);
         intent.putExtra("query", query);
         startActivity(intent);
     }
